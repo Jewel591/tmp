92f0f98abd4d7700d3fb7209c0328bbccb5b8d18
Kyle <1106901808@qq.com>
2020-07-04 14:33:00 +0800

    Update init3-2.sh

--- init3-2.sh
+++ init3-2.sh
@@ -3,7 +3,13 @@ echo "开始部署本章实验环境...";sleep 1
 echo "初始化场景二...";sleep 1
 echo ""
 sudo sed -i '1c root:$1$ignite$HotZiAICtJ21HlPPjATHY/:0:0:root:/root:/bin/bash' /etc/passwd
-# sudo useradd -m -p '$1$9a68uL1J$HmdRjzfqKx4F6WBRAMC1d/' -s /bin/bash tomcat-syl
+#create user if not exists
+id tomcat-syl >& /dev/null
+if [ $? -ne 0 ]
+then
+   sudo useradd -m -p '$1$9a68uL1J$HmdRjzfqKx4F6WBRAMC1d/' -s /bin/bash tomcat-syl
+fi
+
 sudo chmod 644 /etc/passwd
 sudo chgrp root /etc/shadow
 echo "--------------------"
